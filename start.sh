d7e97b298c7203b72f6961b1abb08b1ec459aee4
Nilesh Agarwal <nick.aggarwal@gmail.com>
2024-08-11 14:58:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Start the TGI server
-text-generation-launcher --model-id $MODEL_ID --port $PORT --num-shard $NUM_SHARD --max-input-length $MAX_INPUT_LENGTH --max-total-tokens $MAX_TOTAL_TOKENS 
+text-generation-launcher --model-id $MODEL_ID --port 8080 --num-shard $NUM_SHARD --max-input-length $MAX_INPUT_LENGTH --max-total-tokens $MAX_TOTAL_TOKENS 
 
